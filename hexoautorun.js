12acd7925e55bbab042e277fc2bc27a088c8192e
DF-Master <jiangyida@pku.edu.cn>
2021-03-05 01:57:16 +0800

    删掉了PID公式

--- hexoautorun.js
+++ hexoautorun.js
@@ -1,9 +1,12 @@
 var spawn = require('child_process').spawn;
-free = spawn('hexo', ['server', '-p 4000']);/*其实就是等于执行hexo server -p 4000*/
+free = spawn('hexo', [
+	'run',
+	'-p 4000',
+]); /*其实就是等于执行hexo server -p 4000*/
 free.stdout.on('data', function (data) {
 	console.log('standard output:\n' + data);
 });
-free.stderr.on('data', function (data) { 
+free.stderr.on('data', function (data) {
 	console.log('standard error output:\n' + data);
 });
 free.on('exit', function (code, signal) {
